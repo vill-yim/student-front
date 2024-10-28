9b4c5fd8392d13cf73fb5c8e50c6d0a44357536d
yimco <yimco@gmail.com>
2024-10-28 01:55:59 -0500

    feature: v1 checked

--- src/components/landing/Services/Service.jsx
+++ src/components/landing/Services/Service.jsx
@@ -6,13 +6,14 @@ export const GamifiedModule = () => (
   <div className={styles.serviceCard}>
     <div className={`${styles.cardContent} ${styles.gamified}`}>
       <img
-        src="https://i.ibb.co/wr9J45j/std9.jpg"
+        src="https://i.ibb.co/CHDQ5vB/1service.jpg"
         alt="Módulos Educativos"
         className={styles.serviceImage}
       />
       <div className={styles.gameController}></div>
-      <h3>Módulos Educativos Gamificados</h3>
-      <p>
+
+      <h3 style={{ color: "#2d3748"  }}>Módulos Educativos Gamificados</h3>
+      <p  style={{ color:"#f7fafc" }}>
         Módulos de aprendizaje con minijuegos sobre ahorro, inversión,
         presupuesto y toma de decisiones financieras.
       </p>
@@ -29,7 +30,7 @@ export const BigDataAnalysis = () => (
   <div className={styles.serviceCard}>
     <div className={`${styles.cardContent} ${styles.bigData}`}>
       <img
-        src="https://i.ibb.co/wr9J45j/std9.jpg"
+        src="../../../public/studiantes/2.svg"
         alt="Análisis Big Data"
         className={styles.serviceImage}
       />
@@ -38,8 +39,8 @@ export const BigDataAnalysis = () => (
         <div className={styles.dataPoint}></div>
         <div className={styles.dataPoint}></div>
       </div>
-      <h3>Análisis Personalizado con Big Data</h3>
-      <p>
+      <h3 style={{ color: "#2d3748" }}>Análisis Personalizado con Big Data</h3>
+      <p style={{ color:  "#f7fafc"}}>
         Análisis del progreso de cada estudiante mediante el uso de Big Data,
         con informes detallados para los docentes.
       </p>
@@ -52,15 +53,15 @@ export const FinancialCompetitions = () => (
   <div className={styles.serviceCard}>
     <div className={`${styles.cardContent} ${styles.competitions}`}>
       <img
-        src="https://i.ibb.co/wr9J45j/std9.jpg"
+        src="../../../public/studiantes/3.svg"
         alt="Competencias Financieras"
         className={styles.serviceImage}
       />
       <div className={styles.trophies}>
         <div className={styles.trophy}></div>
       </div>
-      <h3>Competencias y Campeonatos Financieros</h3>
-      <p>
+      <h3 style={{ color: "#2d3748"  }}>Competencias y Campeonatos Financieros</h3>
+      <p  style={{ color:"#f7fafc" }}>
         Organización de competencias a nivel institucional, local, regional y
         nacional para incentivar el aprendizaje.
       </p>
@@ -73,7 +74,7 @@ export const VirtualLibrary = () => (
   <div className={styles.serviceCard}>
     <div className={`${styles.cardContent} ${styles.library}`}>
       <img
-        src="https://i.ibb.co/wr9J45j/std9.jpg"
+        src="../../../public/studiantes/4.svg"
         alt="Biblioteca Virtual"
         className={styles.serviceImage}
       />
@@ -82,8 +83,8 @@ export const VirtualLibrary = () => (
         <span></span>
         <span></span>
       </div>
-      <h3>Biblioteca Virtual de Recursos Financieros</h3>
-      <p>
+      <h3 style={{ color: "#2d3748" }}>Biblioteca Virtual de Recursos Financieros</h3>
+      <p  style={{ color: "#f7fafc" }}> 
         Acceso a videos, artículos y guías prácticas sobre temas financieros,
         inversión y emprendimiento.
       </p>
@@ -96,13 +97,13 @@ export const DigitalCertification = () => (
   <div className={styles.serviceCard}>
     <div className={`${styles.cardContent} ${styles.certification}`}>
       <img
-        src="https://i.ibb.co/wr9J45j/std9.jpg"
+        src="../../../public/studiantes/5.svg"
         alt="Certificación Digital"
         className={styles.serviceImage}
       />
       <div className={styles.badge}></div>
-      <h3>Certificación Digital y Créditos Académicos</h3>
-      <p>
+      <h3 style={{ color: "#2d3748"  }}>Certificación Digital y Créditos Académicos</h3>
+      <p  style={{ color:"#f7fafc" }}>
         Al completar los módulos y actividades, los estudiantes obtienen
         certificaciones digitales reconocidas.
       </p>
